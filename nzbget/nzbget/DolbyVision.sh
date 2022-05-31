814525c92b1f3d246aadac07ab25d852e8aad52e
Brenden Carvalho <BrendenCA@users.noreply.github.com>
2022-05-31 18:35:57 +0000

    fix: DV script for alpine

--- nzbget/nzbget/DolbyVision.sh
+++ nzbget/nzbget/DolbyVision.sh
@@ -14,22 +14,25 @@
 
 POSTPROCESS_SUCCESS=93
 POSTPROCESS_ERROR=94
+MIN_FILE_SIZE=`expr 750 \* 1024`k  # 750MB
 
 if [[ $NZBPP_NZBNAME == *".DV."* ]] && [[ $NZBPP_TOTALSTATUS == "SUCCESS" ]]; then
-  filePath=$(find $NZBPP_DIRECTORY -name '*.mkv' -size +750M | head -n 1)
+  filePath=$(find $NZBPP_DIRECTORY -name '*.mkv' -size +$MIN_FILE_SIZE | head -n 1)
   if [[ $filePath != "" ]]; then
-    workDir=$(mktemp -d)
-    cd $workDir
+    cd $NZBPP_DIRECTORY
     echo "[DETAIL] Extracting Video"
     ffmpeg -hide_banner -loglevel error -i $filePath -c copy video.hevc
     echo "[DETAIL] Extracting Audio"
     ffmpeg -hide_banner -loglevel error -i $filePath -c copy audio.eac3
+    rm $filePath
     mv audio.eac3 audio.ec3
     echo "[DETAIL] Remuxing to mp4"
     mp4muxer --dv-profile 5 -i video.hevc -i audio.ec3 --media-lang eng -o outputfile.mp4
-    mv outputfile.mp4 $NZBPP_DIRECTORY
-    rm $filePath
-    rm -r "$workDir"
+    echo "[DETAIL] Cleaning up"
+    rm video.hevc
+    rm audio.ec3
+    echo "[DETAIL] Renaming output file"
+    mv outputfile.mp4 "${filePath%.*}.mp4"
   fi
 fi
 exit $POSTPROCESS_SUCCESS
